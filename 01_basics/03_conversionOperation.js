8385b5749e9b70a4a836f1ecb2b8ed653fc4471e
SYEDMUJAHEDALI <syedmujahedali055@gmail.com>
2024-05-28 13:06:31 +0000

    conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,14 @@
-let score = "33abc"
+let score = "mujahed"                                        
+//console.log(typeof score);
 
-console.log(typeof score);
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//"33"  => 33
+//"33abc"
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
