044bc99a9be0001ef39f395671077ee65a93b64b
SYEDMUJAHEDALI <syedmujahedali055@gmail.com>
2024-05-29 19:56:18 +0000

    datatypesnotes.js

---
+++ datatypesnotes.js
@@ -0,0 +1,19 @@
+// primitive data types vs non - primitive data types 
+
+// 7 types
+// number ,null,boolean,string,undefined,symbol,BigInt
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+
+// Reference  (Non primitive)
+
+// array ,objects,functions,
